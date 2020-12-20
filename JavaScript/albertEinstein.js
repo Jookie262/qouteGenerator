5fe9ce0f4637e0816653114c17c72f0e74f98b39
Llanz-dev <52281115+Llanz-dev@users.noreply.github.com>
2020-12-20 17:39:26 +0800

    commit

--- JavaScript/albertEinstein.js
+++ JavaScript/albertEinstein.js
@@ -1,22 +1,23 @@
-// let albertEinstein_qoutes = [
-//   "Few are those who see with their own eyes and feel with their own hearts.",
-//   "Imagination is more important than knowledge. Knowledge is limited. Imagination encircles the world.",
-//   "Unthinking respect for authority is the greatest enemy of truth.",
-//   "Try not to become a man of success, but rather try to become a man of value.",
-//   "I am by heritage a Jew, by citizenship a Swiss, and by makeup a human being, and only a human being, without any special attachment to any state1 or national entity whatsoever.",
-//   "Great spirits have always encountered violent opposition from mediocre minds.",
-//   "I would teach peace rather than war. I would inculcate love rather than hate.",
-//   "I believe in intuitions and inspirations. I sometimes feel that I am right. I do not know that I am.",
-//   "Look deep into nature, and then you will understand everything better.",
-//   "All religions, arts and sciences are branches of the same tree.",
-// ];
-// document.getElementById("click").addEventListener("click", () => {
-//   let albertEinstein_words =
-//     albertEinstein_qoutes[
-//       Math.floor(Math.random() * albertEinstein_qoutes.length)
-//     ];
-//   document.querySelector("h1").textContent = "'" + albertEinstein_words + "'";
-// });
+let albertEinstein_qoutes = [
+  "Few are those who see with their own eyes and feel with their own hearts.",
+  "Imagination is more important than knowledge. Knowledge is limited. Imagination encircles the world.",
+  "Unthinking respect for authority is the greatest enemy of truth.",
+  "Try not to become a man of success, but rather try to become a man of value.",
+  "I am by heritage a Jew, by citizenship a Swiss, and by makeup a human being, and only a human being, without any special attachment to any state1 or national entity whatsoever.",
+  "Great spirits have always encountered violent opposition from mediocre minds.",
+  "I would teach peace rather than war. I would inculcate love rather than hate.",
+  "I believe in intuitions and inspirations. I sometimes feel that I am right. I do not know that I am.",
+  "Look deep into nature, and then you will understand everything better.",
+  "All religions, arts and sciences are branches of the same tree.",
+];
+document.getElementById("click").addEventListener("click", () => {
+  let albertEinstein_words =
+    albertEinstein_qoutes[
+      Math.floor(Math.random() * albertEinstein_qoutes.length)
+    ];
+  document.getElementById("qouteText").textContent =
+    '"' + albertEinstein_words + '"';
+});
 let number;
 let state1 = "q";
 document.getElementById("question1").addEventListener("click", () => {
